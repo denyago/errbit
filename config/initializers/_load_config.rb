dc480de3f507edab52abe8f70cb207c6fb159632
Denys Yahofarov <denyago@gmail.com>
2015-06-12 14:00:44 +0200

    * config for mail sending

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -26,16 +26,17 @@ unless defined?(Errbit::Config)
     Errbit::Config.github_secret = ENV['GITHUB_SECRET']
     Errbit::Config.github_org_id = ENV['GITHUB_ORG_ID'] if ENV['GITHUB_ORG_ID']
     Errbit::Config.github_access_scope = ENV['GITHUB_ACCESS_SCOPE'].split(',').map(&:strip) if ENV['GITHUB_ACCESS_SCOPE']
-
-    Errbit::Config.smtp_settings = {
-      :address        => ENV['SMTP_SERVER'] || 'smtp.sendgrid.net',
-      :port           => ENV['SMTP_PORT']   || 25,
-      :authentication => :plain,
-      :user_name      => ENV['SMTP_USERNAME']   || ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SMTP_PASSWORD']   || ENV['SENDGRID_PASSWORD'],
-      :domain         => ENV['SMTP_DOMAIN'] || ENV['SENDGRID_DOMAIN'] || ENV['ERRBIT_EMAIL_FROM'].split('@').last
-    }
   end
+
+  Errbit::Config.smtp_settings = {
+    :address        => ENV['SMTP_SERVER'] || 'smtp.sendgrid.net',
+    :port           => ENV['SMTP_PORT']   || 25,
+    :authentication => :plain,
+    :user_name      => ENV['SMTP_USERNAME']   || ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SMTP_PASSWORD']   || ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SMTP_DOMAIN'] || ENV['SENDGRID_DOMAIN'] || ENV['ERRBIT_EMAIL_FROM'].split('@').last
+  }
+
 
   # Use example config for test environment.
   config_file = Rails.env == "test" ? default_config_file : Rails.root.join("config", "config.yml")
